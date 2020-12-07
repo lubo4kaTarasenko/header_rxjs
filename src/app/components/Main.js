5a3c577f58a5c0efa14cda4b84982c74029c15af
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-12-07 14:15:57 +0200

    scroll

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -9,11 +9,19 @@ export default class Main extends React.Component {
 
     }
   }
-  render() {
 
+  componentDidMount() {
+    console.log('set');
+    //document.onscroll  = ()=>{
+     // this.scrollAnimation()
+   // }
+    window.onscroll = () => { this.scrollAnimation()};
+  }
+
+  render() {
     return (
-      <div className='container'>
-        <Jumbotron fluid>
+      <div className='main_cont'>
+        <Jumbotron fluid id='header'>
           <Container>
            <h1>Super Amazing Header</h1>
           </Container>
@@ -22,6 +30,15 @@ export default class Main extends React.Component {
       </div>
   )}
 
-
+  scrollAnimation(){    
+    const header = document.getElementById('header')
+    const scrollableContainer = document.getElementsByTagName('html')[0];
+      
+    if (scrollableContainer.scrollTop > 50) {
+      header.classList.add("out");
+    } else {
+      header.classList.remove("out");
+    }   
+  }
 
 }
